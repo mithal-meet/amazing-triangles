deec8c2f75ae3daeb64c4c0d4b789ea4056b7292
Mithal Padhy <mithal1996@gmail.com>
2020-06-13 19:26:53 +0530

    drag working

--- can.js
+++ can.js
@@ -2,7 +2,7 @@ var canvas=document.getElementById('box');
 var context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.height = window.innerHeight;
 
 var startCOD = {};
 var endCOD = {};
@@ -10,35 +10,136 @@ var endCOD = {};
 const colors = ['red', 'green', 'blue', 'yellow', 'black'];
 let currentColor = 0;
 
+const triangles = [];
+
+let isDragging = false;
+let triangleBeingDragged = null;
+let triangleDragStartCOD = {};
+
+function sign ( p1,  p2,  p3)
+{
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+function PointInTriangle ( pt,  v1,  v2,  v3)
+{
+    var d1, d2, d3;
+    var has_neg, has_pos;
+
+    d1 = sign(pt, v1, v2);
+    d2 = sign(pt, v2, v3);
+    d3 = sign(pt, v3, v1);
+
+    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(has_neg && has_pos);
+}
+
+function isTrianglePresent(startCOD) {
+    triangles.forEach((triangle, index) => {
+        if (PointInTriangle(startCOD, triangle.startCOD, triangle.leftCOD, triangle.rightCOD)) {
+            isDragging = true;
+            triangleBeingDragged = index;
+            triangleDragStartCOD = startCOD;
+            return false;
+        }
+    })
+    
+    //function trianglepresent)
+}
+
+
+
 document.addEventListener('mousedown', (params) => {
     startCOD.x = params.offsetX;
     startCOD.y = params.offsetY;
+
+    isTrianglePresent(startCOD);
+    console.log(triangles);
 })
+
+function updateDrawing(endCOD) {
+    const diffX = endCOD.x - triangleDragStartCOD.x;
+    const diffY = endCOD.y - triangleDragStartCOD.y;
+
+    const triangleFound = triangles[triangleBeingDragged];
+    const {startCOD, leftCOD, rightCOD, color } = triangleFound;
+
+    triangles[triangleBeingDragged] = {
+        startCOD: {x: startCOD.x + diffX, y: startCOD.y + diffY },
+        leftCOD: {x: leftCOD.x + diffX, y: leftCOD.y + diffY },
+        rightCOD: {x: rightCOD.x + diffX, y: rightCOD.y + diffY },
+        color
+    }
+}
+
 
 document.addEventListener('mouseup', (params) => {
     endCOD.x = params.offsetX;
     endCOD.y = params.offsetY;
 
-    currentColor = (currentColor + 1) % colors.length;
-    draw();
+    if (!isDragging) {
+        drawNewTriangle();
+    } else {
+        updateDrawing(endCOD);
+        isDragging = false;
+    }
+
+    // console.log(triangles)
 })
 
-function draw() {
+//document.addEventListener('mousemove', (params) => {
+    //const currentCOD = {};
+   // currentCOD.x = params.offsetX;
+    // currentCOD.y = params.offsetY;
+    // console.log('current: ', currentCOD)
+    //if (isDragging) {
+    //    updateDrawing(currentCOD);
+    //}
+//})
+
+function drawNewTriangle() {
 
     const leftCOD = { x: startCOD.x - (endCOD.x - startCOD.x), y: endCOD.y};
-    const rightCOD = {x: endCOD.x, y : endCOD.y }
-    
-    
+    const rightCOD = {x: endCOD.x, y : endCOD.y };
+
+    const currentTriangle = {
+        startCOD,
+        leftCOD,
+        rightCOD,
+        color: colors[currentColor]
+    };
+
+    triangles.push(currentTriangle);
+    currentColor = (currentColor + 1) % colors.length;
+
+    startCOD = {};
+}
+
+
+function drawOnCanvas(data) {
+    const { startCOD, leftCOD, rightCOD, color } = data;
     context.beginPath();
     context.moveTo(startCOD.x, startCOD.y);
     context.lineTo(leftCOD.x, leftCOD.y);
     context.lineTo(rightCOD.x, rightCOD.y);
-    context.fillStyle = colors[currentColor];
+    context.fillStyle = color;
     context.fill();
-    
-
 }
- function clearCanvas() {
+
+function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
 }
+
+
+function animate() {
+    clearCanvas();
+    requestAnimationFrame(animate);
+    triangles.forEach(triangle => {
+        drawOnCanvas(triangle)
+    })
+}
+
+animate();
